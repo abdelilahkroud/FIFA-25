bf66bb0de51100b352de8346b3dbb033b8593df0
Abdelilah Kroud <abdelilahkroud77@gmail.com>
2024-11-29 16:59:02 +0100

    EA 25 V1.1

--- EA-FC-25.js
+++ EA-FC-25.js
@@ -1,12 +1,28 @@
 
 const btn = document.getElementById("changeFormation");
 
+    var bool = true;
 
-    btn.addEventListener("click", changeFormation);
+    btn.addEventListener("click", () => {
+        if(bool) {
+            console.log(bool);
+            
+            changeFormation();
+            bool = false;
+        }
+        else{            
+            defaultFormation();
+            bool=true
+        }
+    });
+
+
+  
     
     function changeFormation() {
 
-    document.getElementById("btnFor").innerHTML = "4-3-3";
+    document.getElementById("btnFor").innerHTML = "3-4-3";
+    
 
     const changePostGK = document.getElementById("GK");
     changePostGK.id = "GK-prime";
@@ -41,4 +57,45 @@ const btn = document.getElementById("changeFormation");
     const changePostST = document.getElementById("ST");
     changePostST.id = "ST-prime";
 
+    localStorage.setItem("name", "KROUD");
+
+
 }
+
+    function defaultFormation(){
+
+        document.getElementById("btnFor").innerHTML = "4-3-3";
+        const changePostGK = document.getElementById("GK-prime");
+    changePostGK.id = "GK";
+        
+    const changePostCBL = document.getElementById("CBL-prime");
+    changePostCBL.id = "CBL";
+
+    const changePostCBR = document.getElementById("CBR-prime");
+    changePostCBR.id = "CBR";
+
+    const changePostLB = document.getElementById("LB-prime");
+    changePostLB.id = "LB";
+
+    const changePostRB = document.getElementById("RB-prime");
+    changePostRB.id = "RB";
+
+    const changePostDM = document.getElementById("DM-prime");
+    changePostDM.id = "DM";
+
+    const changePostCML = document.getElementById("CML-prime");
+    changePostCML.id = "CML";
+
+    const changePostCMR = document.getElementById("CMR-prime");
+    changePostCMR.id = "CMR";
+
+    const changePostLW = document.getElementById("LW-prime");
+    changePostLW.id = "LW";
+
+    const changePostRW = document.getElementById("RW-prime");
+    changePostRW.id = "RW";
+
+    const changePostST = document.getElementById("ST-prime");
+    changePostST.id = "ST";
+
+    }
