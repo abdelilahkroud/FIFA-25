21c21f5af59dc534cabda39adf47c0b4331987d8
Abdelilah Kroud <abdelilahkroud77@gmail.com>
2024-11-29 11:44:02 +0100

    edit 2

---
+++ EA-FC-25.js
@@ -0,0 +1,37 @@
+const btn = document.getElementById("changeFormation");
+
+    btn.addEventListener("click", changeFormation);
+
+    function changeFormation() {
+        
+    const changePostCBL = document.getElementById("CBL");
+    changePostCBL.id = "CBL-prime";
+
+    const changePostCBR = document.getElementById("CBR");
+    changePostCBR.id = "CBR-prime";
+
+    const changePostLB = document.getElementById("LB");
+    changePostLB.id = "LB-prime";
+
+    const changePostRB = document.getElementById("RB");
+    changePostRB.id = "RB-prime";
+
+    const changePostDM = document.getElementById("DM");
+    changePostDM.id = "DM-prime";
+
+    const changePostCML = document.getElementById("CML");
+    changePostCML.id = "CML-prime";
+
+    const changePostCMR = document.getElementById("CMR");
+    changePostCMR.id = "CMR-prime";
+
+    const changePostLW = document.getElementById("LW");
+    changePostLW.id = "LW-prime";
+
+    const changePostRW = document.getElementById("RW");
+    changePostRW.id = "RW-prime";
+
+    const changePostST = document.getElementById("ST");
+    changePostST.id = "ST-prime";
+
+}
